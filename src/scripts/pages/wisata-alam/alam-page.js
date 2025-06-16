1721295048cbc0678b1798ce17c412bf502f9cca
rufli <ruflifirdaus@gmail.com>
2025-06-16 17:35:38 +0700

    proyek wisata

--- src/scripts/pages/wisata-alam/alam-page.js
+++
@@ -1,4 +0,0 @@
-
-import KategoriPage from '../kategori/kategori-page';
-
-export default new KategoriPage(1, 'Alam');
--- src/scripts/pages/wisata-religi/religi.js
+++
@@ -1,4 +0,0 @@
-
-import KategoriPage from '../kategori/kategori-page';
-
-export default new KategoriPage(4, 'Religi');
